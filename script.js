d70ea3ac10f54903edaf90f27d10bbdc4c1bd6a6
van khin <khmaister@yahoo.com>
2023-09-16 22:14:17 -0400

    3rd commit start button

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
+// start
+var questionsEl = document.getElementById("questions");
+var answersEl = document.getElementById("choices");
 
-
-
+const startButton = document.createElement("button");
+startButton.textContent = "START";
+document.body.appendChild(startButton);
 
 // countdown timer
-var timeLeft = 60;
+var timeLeft = 10;
 var timerEl = document.getElementById("timer");
-var timerId = setInterval(countdown, 1000);
+startButton.addEventListener("click", () => {
+    var timerId = setInterval(countdown, 1000);
+    startButton.hidden = true;
+    
 
-function countdown() {
-    if (timeLeft == 0){
-        clearTimeout(timerId);
-        //call up end
 
-    } else {
-        timerEl.innerHTML = timeLeft + " seconds remaining";
-        timeLeft--;
-        console.log(timeLeft.valueOf);
+    function countdown() {
+     if (timeLeft == 0){
+            clearTimeout(timerId);
+            //call up end
+
+        } else {
+            timerEl.innerHTML = timeLeft + " seconds remaining";
+            timeLeft--;
+            console.log(timeLeft.valueOf);
+        
+        }
     }
-}
-document.getElementById("timer").textContent = timeLeft;
+});
