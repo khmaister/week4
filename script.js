6a30865d453985be39e228834d44e7808987f80f
van khin <khmaister@yahoo.com>
2023-09-18 18:44:53 -0400

    test functional

--- script.js
+++ script.js
@@ -1,47 +1,64 @@
 // initial load
-
+var userScore = 0;
 var questionsEl = document.getElementById("questions");
 var choicesEl = document.getElementById("choices");
 var highscoreEl = document.getElementById("highscore");
 var initialq = document.getElementById("initial-quote");
-initialq.innerHTML = "You have 60 seconds to try to answer as many questions correct as possible and get the highest score. Every incorrect answer will deduct 4 seconds. Good luck!";
-highscoreEl.innerHTML = "View Highscores";
+initialq.innerHTML = "You have 60 seconds to try to answer as many questions correct as possible and get the highest score. Every incorrect answer will deduct 10 seconds. Good luck!";
+
 var choiceAEl = document.getElementById("choiceA");
 var choiceBEl = document.getElementById("choiceB");
 var choiceCEl = document.getElementById("choiceC");
 var choiceDEl = document.getElementById("choiceD");
+
+
 var ans = document.getElementById("answer-result");
-//document.getElementById("choiUl").style.visibility = false;
+highscoreEl.innerHTML = "View Highscores";
 highscoreEl.addEventListener("click",() =>{
     scores();
 })
+
+
 // start button
 const startButton = document.createElement("button");
 startButton.textContent = "START";
 document.body.appendChild(startButton);
+
+// submit button
+const submitButton = document.createElement("button");
+submitButton.textContent = "Submit";
+document.body.appendChild(submitButton);
+submitButton.hidden = true;
+
 // countdown timer
 
 var timerEl = document.getElementById("timer");
-var timer1 = 0;
+var timer1 = 60;
+if (timer1 == 0){
+    addScore();
+}
 
 
 function timeReduce(){
-    timer1 = timer1 - 4;
+    timer1 = timer1 - 10;
 }
 
 startButton.addEventListener("click", () => {
     var timerId = setInterval(countdown, 1000);
     console.log("quiz started");
     startButton.hidden = true;
+    submitButton.hidden= true;
     initialq.innerHTML = "";
     highscoreEl.innerHTML = "";
+    popVal=0;
     populate();
     timer1 = 60;
+    userScore = 0;
     
     
     function countdown() {
         timerEl.innerHTML = timer1 + " seconds remaining";
-        if (timer1 == 0){
+        if (timer1 <= 0){
             clearTimeout(timerId);
             addScore();
 
@@ -56,121 +73,158 @@ startButton.addEventListener("click", () => {
 
 function que1(){
     questionsEl.innerHTML = 'Which of the following is not a front-end technology?';
-    //choiceAEl = {name: "A. HTML", answer: false,};
-    choiceAEl.innerHTML = "A. HTML";
-    choiceAEl.addEventListener("click", ()=> {
-        console.log("clickable");
-        ans.innerHTML = "incorrect";
-        timeReduce();
-    })
-    //choiceBEl = {name: "B. CSS", answer: false,};
-    choiceBEl.innerHTML = "B. CSS";
-    choiceBEl.addEventListener("click", ()=> {
-        console.log("clickable");
-        ans.innerHTML = "incorrect";
-        timeReduce();
-    })
-    //choiceCEl = {name: "C. JavaScript", answer: false,}
-    choiceCEl.innerHTML = "C. Javascript";
-    choiceCEl.addEventListener("click", ()=> {
-        console.log("clickable");
-        ans.innerHTML = "incorrect";
-        timeReduce();
-    })
-    //choiceDEl = {name: "D. SQL", answer: true,}*/
-    choiceDEl.innerHTML = "D. SQL";
-    choiceDEl.addEventListener("click", ()=> {
-        console.log("clickable");
-        ans.innerHTML = "correct";
-        populate();
-    })
     
-}
-function que2(){
-    questionsEl.innerHTML = 'Amongst which of the following programming language is used as a server-side language?';
-    choiceAEl.innerHTML = "A. Pyhton";
-    choiceAEl.addEventListener("click", ()=> {
-        console.log("clickable");
-        ans.innerHTML = "incorrect";
+    const que1Abutton = document.createElement("choiceA");
+    que1Abutton.textContent = "A. HTML";
+    que1Abutton.addEventListener("click", () => {
         timeReduce();
-    })
-    choiceBEl.innerHTML = "B. C++";
-    choiceBEl.addEventListener("click", ()=> {
-        console.log("clickable");
         ans.innerHTML = "incorrect";
-        timeReduce();
-    })
-    choiceCEl.innerHTML = "C. Javascript";
-    choiceCEl.addEventListener("click", ()=> {
         console.log("clickable");
+    })
+    const que1Bbutton = document.createElement("choiceB");
+    que1Bbutton.textContent = "B. Css";
+    que1Bbutton.addEventListener("click", () => {
+        timeReduce();
         ans.innerHTML = "incorrect";
-        timeReduce();
-    })
-    //choiceDEl = {name: "D. SQL", answer: true,}*/
-    choiceDEl.innerHTML = "D. Both A and B";
-    choiceDEl.addEventListener("click", ()=> {
         console.log("clickable");
+    })
+    const que1Cbutton = document.createElement("choiceC");
+    que1Cbutton.textContent = "C. JavaScript";
+    que1Cbutton.addEventListener("click", () => {
+        timeReduce();
+        ans.innerHTML = "incorrect";
+        console.log("clickable");
+    })
+    const que1Dbutton = document.createElement("choiceD");
+    que1Dbutton.textContent = "D. SQL";
+    que1Dbutton.addEventListener("click", () => {
         ans.innerHTML = "correct";
+        console.log("clickable");
         populate();
+        userScore = userScore + timer1;
     })
-}
-function que3(){
-    questionsEl.innerHTML = 'What is Git?';
-    choiceAEl.innerHTML = "A. Framework";
-    choiceAEl.addEventListener("click", ()=> {
-        console.log("clickable");
-        ans.innerHTML = "incorrect";
-        timeReduce();
-    })
-    choiceBEl.innerHTML = "B. Version control system";
-    choiceBEl.addEventListener("click", ()=> {
-        console.log("clickable");
-        ans.innerHTML = "correct";
-        populate();
-        
-    })
-    choiceCEl.innerHTML = "C. Database";
-    choiceCEl.addEventListener("click", ()=> {
-        console.log("clickable");
-        ans.innerHTML = "incorrect";
-        timeReduce();
-    })
-    choiceDEl.innerHTML = "D. Package manager";
-    choiceDEl.addEventListener("click", ()=> {
-        console.log("clickable");
-        ans.innerHTML = "incorrect";
-        timeReduce();
-    })
-}
-function que4(){
-    questionsEl.innerHTML = 'Amongst which of the follwing protocol is used to exchange the data between client and server?';
-    choiceAEl.innerHTML = "A. HTTP";
-    choiceAEl.addEventListener("click", ()=> {
-        console.log("clickable");
-        ans.innerHTML = "correct";
-        populate();
-    })
-    choiceBEl.innerHTML = "B. TCP/IP";
-    choiceBEl.addEventListener("click", ()=> {
-        console.log("clickable");
-        ans.innerHTML = "incorrect";
-        timeReduce();
-        
-    })
-    choiceCEl.innerHTML = "C. SMTP";
-    choiceCEl.addEventListener("click", ()=> {
-        console.log("clickable");
-        ans.innerHTML = "incorrect";
-        timeReduce();
-    })
-    choiceDEl.innerHTML = "D. FTP";
-    choiceDEl.addEventListener("click", ()=> {
-        console.log("clickable");
-        ans.innerHTML = "incorrect";
-        timeReduce();
-    })
+    if (popVal==1){
+    document.getElementById("choiceA").appendChild(que1Abutton);
+    document.getElementById("choiceB").appendChild(que1Bbutton);
+    document.getElementById("choiceC").appendChild(que1Cbutton);
+    document.getElementById("choiceD").appendChild(que1Dbutton);
+    }
 }
 
+function que2(){
+    questionsEl.innerHTML = 'What is git?';
+    
+    const que2Abutton = document.createElement("choiceA");
+    que2Abutton.textContent = "A. Framework";
+    que2Abutton.addEventListener("click", () => {
+        timeReduce();
+        ans.innerHTML = "incorrect";
+        console.log("clickable");
+    })
+    const que2Bbutton = document.createElement("choiceB");
+    que2Bbutton.textContent = "B. Version control system";
+    que2Bbutton.addEventListener("click", () => {
+        populate();
+        ans.innerHTML = "correct";
+        console.log("clickable");
+        userScore = userScore + timer1;
+    })
+    const que2Cbutton = document.createElement("choiceC");
+    que2Cbutton.textContent = "C. Database";
+    que2Cbutton.addEventListener("click", () => {
+        timeReduce();
+        ans.innerHTML = "incorrect";
+        console.log("clickable");
+    })
+    const que2Dbutton = document.createElement("choiceD");
+    que2Dbutton.textContent = "D. Package manager";
+    que2Dbutton.addEventListener("click", () => {
+        ans.innerHTML = "incorrect";
+        console.log("clickable");
+        timeReduce();
+    })
+    document.getElementById("choiceA").appendChild(que2Abutton);
+    document.getElementById("choiceB").appendChild(que2Bbutton);
+    document.getElementById("choiceC").appendChild(que2Cbutton);
+    document.getElementById("choiceD").appendChild(que2Dbutton);
+    
+}
+function que3(){
+    questionsEl.innerHTML = 'In an HTML document, which tag is used to add JavaScript code?';
+    
+    const que3Abutton = document.createElement("choiceA");
+    que3Abutton.textContent = "A. <javascript>";
+    que3Abutton.addEventListener("click", () => {
+        timeReduce();
+        ans.innerHTML = "incorrect";
+        console.log("clickable");
+    })
+    const que3Bbutton = document.createElement("choiceB");
+    que3Bbutton.textContent = "B. <js>";
+    que3Bbutton.addEventListener("click", () => {
+        timeReduce();
+        ans.innerHTML = "incorrect";
+        console.log("clickable");
+    })
+    const que3Cbutton = document.createElement("choiceC");
+    que3Cbutton.textContent = "C. <script>";
+    que3Cbutton.addEventListener("click", () => {
+        populate();
+        ans.innerHTML = "correct";
+        console.log("clickable");
+        userScore = userScore + timer1
+    })
+    const que3Dbutton = document.createElement("choiceD");
+    que3Dbutton.textContent = "D. <java>";
+    que3Dbutton.addEventListener("click", () => {
+        ans.innerHTML = "incorrect";
+        console.log("clickable");
+        timeReduce();
+    })
+    document.getElementById("choiceA").appendChild(que3Abutton);
+    document.getElementById("choiceB").appendChild(que3Bbutton);
+    document.getElementById("choiceC").appendChild(que3Cbutton);
+    document.getElementById("choiceD").appendChild(que3Dbutton);
+    
+}
+function que4(){
+    questionsEl.innerHTML = 'Which of the following is an array method in JavaScript?';
+    
+    const que4Abutton = document.createElement("choiceA");
+    que4Abutton.textContent = "A. map";
+    que4Abutton.addEventListener("click", () => {
+        timeReduce();
+        ans.innerHTML = "incorrect";
+        console.log("clickable");
+    })
+    const que4Bbutton = document.createElement("choiceB");
+    que4Bbutton.textContent = "B. every";
+    que4Bbutton.addEventListener("click", () => {
+        timeReduce();
+        ans.innerHTML = "incorrect";
+        console.log("clickable");
+    })
+    const que4Cbutton = document.createElement("choiceC");
+    que4Cbutton.textContent = "C. reduce";
+    que4Cbutton.addEventListener("click", () => {
+        timeReduce();
+        ans.innerHTML = "incorrect";
+        console.log("clickable");
+    })
+    const que4Dbutton = document.createElement("choiceD");
+    que4Dbutton.textContent = "D. all of the above";
+    que4Dbutton.addEventListener("click", () => {
+        ans.innerHTML = "correct";
+        console.log("clickable");
+        populate();
+        userScore = userScore +timer1
+    })
+    document.getElementById("choiceA").appendChild(que4Abutton);
+    document.getElementById("choiceB").appendChild(que4Bbutton);
+    document.getElementById("choiceC").appendChild(que4Cbutton);
+    document.getElementById("choiceD").appendChild(que4Dbutton);
+    
+}
 // clear all fields
 function cleanSlate(){
     questionsEl.innerHTML = "";
@@ -181,14 +235,19 @@ function cleanSlate(){
     choiceCEl.innerHTML = "";
     choiceDEl.innerHTML = "";
     ans.innerHTML = "";
-    timer1 = 0;
-    done1 = false;
-    done2 = false;
-    done3 = false;
-    done4 = false;
+    timer1 = 0, false;
     
+
+    
+
 }
 
+function removeListeners(){
+    choiceAEl.innerHTML = "";
+    choiceBEl.innerHTML = "";
+    choiceCEl.innerHTML = "";
+    choiceDEl.innerHTML = "";
+}
 
 
 
@@ -196,46 +255,45 @@ function cleanSlate(){
 function scores(){
     cleanSlate();
     initialq.innerHTML = "Highscores";
+    return;
 
 }
+var inputEl = document.getElementById("imput");
 
 //adding score
 function addScore(){
     cleanSlate();
-    initialq.innerHTML = "Your score is " ;
+    initialq.innerHTML = "Your score is " + userScore ;
     startButton.hidden = false;
     highscoreEl.innerHTML = "Highscores";
+    submitButton.hidden = false;
+
 
 
 }
-/*var variable = 0;
-function random(){
-    let variable = Math.floor((Math.random()*2)+1);
-    console.log(variable);
-    populate();
 
-}*/
-
-// populate question area
-var done1 = false;
-var done2 = false;
-var done3 = false;
-var done4 = false;
+var popVal = 0;
 function populate(){
     console.log("pop")
-    if (done1 == false){
-        done1 = true;
+    if (popVal == 0){
+        popVal = popVal + 1;
+        
         que1();        
-    } else if(done2 == false){        
-        done2 = true;
+    } else if(popVal == 1){  
+        removeListeners();     
+        popVal = popVal+1;
         que2();
-    } else if(done3 == false){        
-        done3 = true;
+    } else if(popVal == 2){ 
+        removeListeners();       
+        popVal = popVal+1;
         que3();
-    } else if(done4 == false){        
-        done4 = true;
+    } else if(popVal == 3){  
+        removeListeners();      
+        popVal = popVal+1;
         que4();
     }else {
         addScore();
     }
+    
 }
+
